8502725d32aca3ca03918f8e67e3f02d25aadb26
Rushan Kashapov <kashapovrush@mail.ru>
2023-01-05 15:54:50 +0300

    release 1.0 (05.01.2023)

---
+++ app/src/main/java/com/kashapovrush/godrivekotlin/utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.kashapovrush.godrivekotlin.utilities
+
+class Constants {
+    companion object {
+        const val KEY_COLLECTION_USERS = "users"
+        const val KEY_PREFERENCE_NAME = "chatAppPreference"
+    }
+}
